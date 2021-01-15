5b6293e326b82e423563cabbd1174e98866944f5
Electogenius <sndeepak@icloud.com>
2021-01-15 16:54:26 +0530

    added better text system + font

--- Hyfon.js
+++ Hyfon.js
@@ -1,126 +1,143 @@
 function hyfon_string(a, sysname) {
-   var usemes = {}
-   a = a.replace(/sys/gi, sysname)
+	var usemes = {}
+	a = a.replace(/sys/gi, sysname)
 
-   var aa; //current elm name for hue command
-   var j = ''; //js code
-   var name; //appname
-   var mbt = '';
-   a = a.replace(/(\r\n|\n|\r)/gm, "");
-   a = a.split(';');
-   //for every command
-   for (l in a) {
-      var c; //command buffer
-      var r = a[l].split('-')
+	var aa; //current elm name for hue command
+	var j = ''; //js code
+	var name; //appname
+	var mbt = '';
+	a = a.replace(/(\r\n|\n|\r)/gm, "");
+	a = a.split(';');
+	//for every command
+	for (l in a) {
+		var c; //command buffer
+		var r = a[l].split('-')
 
-      for (qq in r) {
-         k = r[qq]
-         if (mbt == 'add') {
-            if (qq == 1) {
-               j += `${k}.appendChild(`
-            } else {
-               mbt = ''
-               j += `${k})`
-            }
-            c = ''
-         }
-         if (mbt == "style") {
-            if (qq == 1) {
-               usemes.el = k
-            } else {
-               mbt = '';
-               console.log(k);
-               var C = JSON.parse(k);
-               for (value in C) {
-                  j += `${usemes.el}.style.${value} = "${C[value]}"; `
-               }
-            }
-            c = ''
-         }
-         //if this is the first keyword,
-         if (qq == 0) {
-            //switch keyword
-            switch (k) {
-               case '/*':
-                  c = 'comment'
-                  j += '//'
-                  break;
-               case "@NAME":
-                  c = 'name'
-                  break;
-
-               case 'elm':
-                  j += 'var ';
-                  c = 'elm';
-                  break;
-               case 'var':
-                  c = 'var'
-                  break;
-               case 'add':
-                  mbt = 'add'
-                  break;
-               case 'style':
-                  mbt = 'style'
-                  break;
-               default:
-                  break;
-            }
-         } else { //for 2 word commands:
-            switch (c) {
-               case 'name':
-                  name = k;
-                  alert('name: ' + k)
-                  break;
-               case 'type':
-                  break
-               default:
-                  break;
-            }
-         }
-         //hue
-         if (c == 'elm') {
-            if (qq == 1) {
-               j += k
-               aa = k
-            }
-            if (qq == 2) {
-               j += ` = document.createElement(${k})`
-            }
-            if (qq == 3) {
-               j += `\n{var n = document.createTextNode(${k})\n${aa}.appendChild(n)}`;
-            }
-         }
-         //var
-         if (c == 'var') {
-            if (qq == 0) {
-               j += 'var '
-            }
-            if (qq == 1) {
-               j += k
-            }
-            if (qq == 2) {
-               j += ` = ${k}`
-            }
-         }
-      }
-      j += '; '
-      aa = ''
-   }
-   j = j.replace('; ', '\n');
-   j = j.replace(/__/g, "-")
-   var result = j
-   return result;
+		for (qq in r) {
+			k = r[qq]
+			if (mbt == 'add') {
+				if (qq == 1) {
+					j += `${k}.appendChild(`
+				} else {
+					mbt = ''
+					j += `${k})`
+				}
+				c = ''
+			}
+			if (mbt == "style") {
+				if (qq == 1) {
+					usemes.el = k
+				} else {
+					mbt = '';
+					////(k);
+					var C = JSON.parse(k);
+					for (value in C) {
+						j += `${usemes.el}.style.${value} = "${C[value]}"; `
+					}
+				}
+				c = ''
+			}
+			if (mbt == 'onclick') {
+				if (qq == 1) {
+					j += `${k}.onclick = function(){`
+					mbt = ''
+				}
+			}
+			//if this is the first keyword,
+			if (qq == 0) {
+				//switch keyword
+				switch (k) {
+					case '/*':
+						c = 'comment'
+						break;
+					case "@NAME":
+						c = 'name'
+						break;
+					case 'click':
+						mbt = 'onclick'
+						c = ''
+						break;
+					case 'elm':
+						j += 'var ';
+						c = 'elm';
+						break;
+					case 'var':
+						c = 'var'
+						break;
+					case 'add':
+						mbt = 'add'
+						break;
+					case 'style':
+						mbt = 'style'
+						break;
+					case '}':
+						j += '}'
+						break;
+					case 'set':
+						mbt = 'set'
+						break;
+					default:
+						break;
+				}
+			} else { //for 2 word commands:
+				switch (c) {
+					case 'name':
+						name = k;
+						alert('name: ' + k)
+						break;
+					case 'type':
+						break
+					default:
+						break;
+				}
+			}
+			//hue
+			if (c == 'elm') {
+				if (qq == 1) {
+					j += k
+					aa = k
+				}
+				if (qq == 2) {
+					j += ` = document.createElement(${k})`
+				}
+				if (qq == 3) {
+					j += `\n{var n = ${k}; n = n.replace(/</, '&lt;'); n = n.replace(/>/,'&gt;');${aa}.innerHTML = ${k}}`;
+				}
+			}
+			//var
+			if (c == 'var') {
+				if (qq == 0) {
+					j += 'var '
+				}
+				if (qq == 1) {
+					j += k
+				}
+				if (qq == 2) {
+					j += ` = ${k}`
+				}
+			}
+		}
+		j += '; '
+		aa = ''
+	}
+	j = j.replace('; ', '\n');
+	j = j.replace(/__/g, "-");
+	j = j.replace(/_n/g, "<br>");
+	var result = j
+	return result;
 }
-function hyfon_run_filename(filename, sysname){
-   var r = true
-   var c = new XMLHttpRequest();
-   c.open('GET', `${filename}.hy`);
-   c.onreadystatechange = function(){
-      if(c.responseText !== '' && r == true){
-      console.log(c.responseText)
-      eval(hyfon_string(c.responseText, sysname))
-      r = !r
-      }
-   };
-   c.send();
+
+function hyfon_run_filename(filename, sysname) {
+	var r = true
+	var c = new XMLHttpRequest();
+	c.open('GET', `${filename}.hy`);
+	c.onreadystatechange = function() {
+		if (c.responseText !== '' && r == true) {
+			////(c.responseText)
+			////(hyfon_string(c.responseText, sysname))
+			eval(hyfon_string(c.responseText, sysname))
+			r = !r
+		}
+	};
+	c.send();
 }
--- demo.hy
+++ demo.hy
@@ -1,5 +1,5 @@
 elm-heading-'h1'-'Hyfon';
-style-sys-{"fontFamily": "Roboto,sans__Serif", 
+style-sys-{"fontFamily": "ui__rounded", 
 "backgroundColor": "black", 
 "color": "white", 
 "position": "absolute", 
@@ -7,10 +7,15 @@ style-sys-{"fontFamily": "Roboto,sans__Serif",
 "height": "100vh",
 "top": 0,
 "left": 0};
-elm-info-'p'-'Hyfon is an unusual programming language I made.';
+elm-info-'p'-
+`Hyfon is an unusual programming language I made. _n It\'s used for web developing, and as the name suggests, hyphens are important and indicate the next keyword.`;
 elm-ff-'p'-'Fun fact: This page was written in Hyfon!';
+click-ff-{;
+elm-l-'p'-"bruh why'd you click the fun fact";
+add-sys-l;
+};
 style-ff-{"borderLeft": "5px solid blue", "backgroundColor": "#335"};
 add-sys-heading;
-/*This is a comment, comments end with semicolons*/;
+/*-This is a comment, comments end with semicolons*/;
 add-sys-info;
 add-sys-ff;
